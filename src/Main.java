d5e39a19d15b0deaaacf04da68edee4e9d1cc534
k7dus <100138253@lubbockisd.net>
2022-09-01 09:37:57 -0500

    CAT

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,17 @@ public class Main {
     public static void main
             (String[] args) {
         System.out.println("Hello world!");
+        System.out.println("           .'\\   /`.");
+        System.out.println("         .'.-.`-'.-.`.");
+        System.out.println("    ..._:   .-. .-.   :_...");
+        System.out.println("  .'    '-.(o ) (o ).-'    `.");
+        System.out.println(" :  _    _ _`~(_)~`_ _    _  :");
+        System.out.println(":  /:   ' .-=_   _=-. `   ;\\  :");
+        System.out.println(":   :|-.._  '     `  _..-|:   :");
+        System.out.println(" :   `:| |`:-:-.-:-:'| |:'   :");
+        System.out.println("  `.   `.| | | | | | |.'   .'");
+        System.out.println("   `.   `-:_| | |_:-'   .'");
+        System.out.println(" jgs  `-._   ````    _.-'");
+        System.out.println("          ``-------''");
     }
 }
